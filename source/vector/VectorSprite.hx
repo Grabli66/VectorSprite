7f0b86c677488b240574d96909fa634869e2968c
Grabli66 <grabli66@gmail.com>
2019-02-07 19:07:49 +0300

    Text

--- source/vector/VectorSprite.hx
+++ source/vector/VectorSprite.hx
@@ -1,167 +1,190 @@
 package vector;
 
+import flixel.text.FlxText;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import haxe.xml.Access;
 
-import flixel.util.FlxSpriteUtil;
-
 class VectorSprite extends FlxSprite {
-    /**
-     * Alpha scale to recalculate it to 0..1
-     */
-    public inline static final ALPHA_SCALE = 255;
+	/**
+	 * Alpha scale to recalculate it to 0..1
+	 */
+	public inline static final ALPHA_SCALE = 255;
 
-    /**
-     * Sprite xml data
-     */
-    var xml:Access;
+	/**
+	 * Sprite xml data
+	 */
+	var xml:Access;
 
-    /**
-     * Registered brushes
-     */
-    var brushes = new Map<String, Brush>();
+	/**
+	 * Registered brushes
+	 */
+	var brushes = new Map<String, Brush>();
 
-    /**
-     * Apply brush for drawing if it exists and is not current (TODO)
-     */
-    function applyBrush(name:String) {
-        var brush = brushes[name];
-        var color = FlxColor.fromInt(brush.color);    
-        var alpha = brush.alpha / ALPHA_SCALE;
-        DrawHelper.beginFill(color, alpha);
-    }
+	/**
+	 * Apply brush for drawing if it exists and is not current (TODO)
+	 */
+	function applyBrush(name:String) {
+		var brush = brushes[name];
+		var color = FlxColor.fromInt(brush.color);
+		var alpha = brush.alpha / ALPHA_SCALE;
+		DrawHelper.beginFill(color, alpha);
+	}
 
-    /**
-     * Create rect
-     */
-    function createRect(elem:haxe.xml.Access) {
-        var x = Std.parseInt(elem.att.x);
-        var y = Std.parseInt(elem.att.y);
-        var width = Std.parseInt(elem.att.width);
-        var height = Std.parseInt(elem.att.height);
-        applyBrush(elem.att.brush);
+	/**
+	 * Create rect
+	 */
+	function createRect(elem:haxe.xml.Access) {
+		var x = Std.parseInt(elem.att.x);
+		var y = Std.parseInt(elem.att.y);
+		var width = Std.parseInt(elem.att.width);
+		var height = Std.parseInt(elem.att.height);
+		applyBrush(elem.att.brush);
 
-        DrawHelper.drawRect(x, y, width, height);
-        DrawHelper.endFill();
-        DrawHelper.commit(this);
-    }
+		DrawHelper.drawRect(x, y, width, height);
+		DrawHelper.endFill();
+		DrawHelper.commit(this);
+	}
 
-    /**
-     * Create circle
-     */
-    function createCircle(elem:haxe.xml.Access) {
-        var x = Std.parseInt(elem.att.x);
-        var y = Std.parseInt(elem.att.y);
-        var radius = Std.parseInt(elem.att.radius);
-        applyBrush(elem.att.brush);
-        
-        DrawHelper.drawCircle(x, y, radius);
-        DrawHelper.endFill();
-        DrawHelper.commit(this);
-    }
+	/**
+	 * Create circle
+	 */
+	function createCircle(elem:haxe.xml.Access) {
+		var x = Std.parseInt(elem.att.x);
+		var y = Std.parseInt(elem.att.y);
+		var radius = Std.parseInt(elem.att.radius);
+		applyBrush(elem.att.brush);
 
-    /**
-     * Create ellipse
-     */
-    function createEllipse(elem:haxe.xml.Access) {
-        var x = Std.parseInt(elem.att.x);
-        var y = Std.parseInt(elem.att.y);
-        var width = Std.parseInt(elem.att.width);
-        var height = Std.parseInt(elem.att.height);
-        applyBrush(elem.att.brush);
-        
-        DrawHelper.drawEllipse(x, y, width, height);
-        DrawHelper.endFill();
-        DrawHelper.commit(this);
-    }
+		DrawHelper.drawCircle(x, y, radius);
+		DrawHelper.endFill();
+		DrawHelper.commit(this);
+	}
 
-    /**
-     * Create path
-     */
-    function createPath(elem:haxe.xml.Access) {
-        var data = elem.att.data;
-        var items = data.split(" ");
+	/**
+	 * Create ellipse
+	 */
+	function createEllipse(elem:haxe.xml.Access) {
+		var x = Std.parseInt(elem.att.x);
+		var y = Std.parseInt(elem.att.y);
+		var width = Std.parseInt(elem.att.width);
+		var height = Std.parseInt(elem.att.height);
+		applyBrush(elem.att.brush);
 
-        applyBrush(elem.att.brush);
+		DrawHelper.drawEllipse(x, y, width, height);
+		DrawHelper.endFill();
+		DrawHelper.commit(this);
+	}
 
-        for (item in items) {
-            var points = item.split(",");
-            var first = points[0];
-            var tp = first.charAt(0); // type of curve                        
-            switch tp {
-                case "m":
-                    var x = Std.parseInt(first.substr(1, first.length));
-                    var y = Std.parseInt(points[1]);
-                    DrawHelper.moveTo(x, y);
-                case "l":
-                    var x = Std.parseInt(first.substr(1, first.length));
-                    var y = Std.parseInt(points[1]);
-                    DrawHelper.lineTo(x, y);
-                case "q":
-                    var cx = Std.parseInt(first.substr(1, first.length));
-                    var cy = Std.parseInt(points[1]);
-                    var ax = Std.parseInt(points[2]);
-                    var ay = Std.parseInt(points[3]);
-                    DrawHelper.quadraticCurveTo(cx, cy, ax, ay);
-                case "c":
-                    var cx = Std.parseInt(first.substr(1, first.length));
-                    var cy = Std.parseInt(points[1]);
-                    var cx1 = Std.parseInt(points[2]);
-                    var cy1 = Std.parseInt(points[3]);
-                    var ax = Std.parseInt(points[4]);
-                    var ay = Std.parseInt(points[5]);
-                    DrawHelper.cubicCurveTo(cx, cy, cx1, cy1, ax, ay);
-            }
-        }
+	/**
+	 * Create path
+	 */
+	function createPath(elem:haxe.xml.Access) {
+		var data = elem.att.data;
+		var items = data.split(" ");
 
-        DrawHelper.endFill();
-        DrawHelper.commit(this);
-    }
+		applyBrush(elem.att.brush);
 
-    /**
-     * Create brush
-     */
-    function createBrush(elem:haxe.xml.Access) {
-        var name = elem.att.name;
-        var color = Std.parseInt("0x" + elem.att.color);
-        var brush:Brush = {
-            name: name,
-            color: color & 0xFFFFFF,
-            alpha: (color >> 24) & 0xFF
-        };
+		for (item in items) {
+			var points = item.split(",");
+			var first = points[0];
+			var tp = first.charAt(0); // type of curve
+			switch tp {
+				case "m":
+					var x = Std.parseInt(first.substr(1, first.length));
+					var y = Std.parseInt(points[1]);
+					DrawHelper.moveTo(x, y);
+				case "l":
+					var x = Std.parseInt(first.substr(1, first.length));
+					var y = Std.parseInt(points[1]);
+					DrawHelper.lineTo(x, y);
+				case "q":
+					var cx = Std.parseInt(first.substr(1, first.length));
+					var cy = Std.parseInt(points[1]);
+					var ax = Std.parseInt(points[2]);
+					var ay = Std.parseInt(points[3]);
+					DrawHelper.quadraticCurveTo(cx, cy, ax, ay);
+				case "c":
+					var cx = Std.parseInt(first.substr(1, first.length));
+					var cy = Std.parseInt(points[1]);
+					var cx1 = Std.parseInt(points[2]);
+					var cy1 = Std.parseInt(points[3]);
+					var ax = Std.parseInt(points[4]);
+					var ay = Std.parseInt(points[5]);
+					DrawHelper.cubicCurveTo(cx, cy, cx1, cy1, ax, ay);
+			}
+		}
 
-        brushes[name] = brush;
-    }
+		DrawHelper.endFill();
+		DrawHelper.commit(this);
+	}
 
-    function redraw() {
-        var width = Std.parseInt(xml.node.sprite.att.width);
-        var height = Std.parseInt(xml.node.sprite.att.height);
-        makeGraphic(width, height, FlxColor.TRANSPARENT);
+	/**
+	 * Create text element
+	 */
+	function createText(elem:haxe.xml.Access) {
+		var x = Std.parseInt(elem.att.x);
+		var y = Std.parseInt(elem.att.y);
+        var size = 8;
+        var color = 0xFFFFFF;
 
-        for (elem in xml.node.sprite.elements) {
-            switch elem.name {
-                case "brush":
-                    createBrush(elem);
-                case "rect":
-                    createRect(elem);
-                case "circle":
-                    createCircle(elem);
-                case "ellipse":
-                    createEllipse(elem);
-                case "path":
-                    createPath(elem);
-            }
-        }
-    }
+        if (elem.has.size)
+            size = Std.parseInt(elem.att.size);
 
-    /**
-     * Constructor
-     */
-    public function new(text:String) {
-        super();        
-        xml = new haxe.xml.Access(Xml.parse(text));
+        if (elem.has.color)
+            color = Std.parseInt("0x" + elem.att.color);        
 
-        redraw();
-    }
+		var text = elem.att.text;
+		var flText = new FlxText(x, y, 0, text);
+        flText.setFormat(null, size, FlxColor.GREEN);
+        flText.drawFrame(true);
+		pixels.draw(flText.pixels);
+	}
+
+	/**
+	 * Create brush
+	 */
+	function createBrush(elem:haxe.xml.Access) {
+		var name = elem.att.name;
+		var color = Std.parseInt("0x" + elem.att.color);
+		var brush:Brush = {
+			name: name,
+			color: color & 0xFFFFFF,
+			alpha: (color >> 24) & 0xFF
+		};
+
+		brushes[name] = brush;
+	}
+
+	function redraw() {
+		var width = Std.parseInt(xml.node.sprite.att.width);
+		var height = Std.parseInt(xml.node.sprite.att.height);
+		makeGraphic(width, height, FlxColor.TRANSPARENT);
+
+		for (elem in xml.node.sprite.elements) {
+			switch elem.name {
+				case "brush":
+					createBrush(elem);
+				case "rect":
+					createRect(elem);
+				case "circle":
+					createCircle(elem);
+				case "ellipse":
+					createEllipse(elem);
+				case "path":
+					createPath(elem);
+				case "text":
+					createText(elem);
+			}
+		}
+	}
+
+	/**
+	 * Constructor
+	 */
+	public function new(text:String) {
+		super();
+		xml = new haxe.xml.Access(Xml.parse(text));
+
+		redraw();
+	}
 }
--- test/TestState.hx
+++ test/TestState.hx
@@ -1,4 +1,6 @@
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class TestState extends FlxState
 {
@@ -15,16 +17,17 @@ class TestState extends FlxState
 			<sprite width="100" height="100">
 				<brush name="red" color="FFFF0033" />
 				<brush name="blue" color="FFAA0033" />				
-				<brush name="green" color="FF00FF33" />	
+				<brush name="green" color="FF00FF33" />
 				<brush name="grey" color="33000000" />
-				<circle x="50" y="50" radius="50" brush="red" />			
+				<circle x="50" y="50" radius="50" brush="red" />
 				<path data="m0,0 l10,10 l10,20 c20,20,100,80,80,80 l0,70, l0,0" brush="blue" />
 				<ellipse x="31" y="53" width="50" height="30" brush="grey" />
 				<ellipse x="30" y="50" width="50" height="30" brush="green" />
-				<ellipse x="35" y="55" width="45" height="25" brush="grey" />
+				<ellipse x="35" y="55" width="45" height="25" brush="grey" />			
+				<text x="10" y="20" size="14" color="00FF33" text="Hello world" />
 			</sprite>
 		');
-		
+				
 		// <rect x="0" y="0" width="100" height="100" brush="red" />
 		// <circle x="50" y="50" radius="50" brush="blue" />
 
@@ -32,7 +35,8 @@ class TestState extends FlxState
 		sprite.x = 300;
 		sprite.y = 300;
 		sprite.antialiasing = true;
-		//sprite.scale.set(2,2);		
+		
+		sprite.scale.set(2,2);		
 	}
 
 	/**
